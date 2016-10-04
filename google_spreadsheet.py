c4c6eab3d86e08998d7accd63b3921d2be49249a
Brandon <bsmith393@gatech.edu>
2016-10-04 14:44:11 -0400

    Added google spreadsheet submit

---
+++ google_spreadsheet.py
@@ -0,0 +1,58 @@
+from oauth2client.service_account import ServiceAccountCredentials as SAC
+from apiclient import discovery
+import httplib2
+import string
+
+
+class Sheet(object):
+    sac_file = 'google-sheets-key.json' # Service account credentials file
+
+
+    def __init__(self, sheet_id):
+        ''' Authenticate with a service account '''
+        credentials = SAC.from_json_keyfile_name(self.sac_file,
+            scopes=['https://www.googleapis.com/auth/spreadsheets']
+        )
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('sheets', 'v4', http=http)
+        self.sheet = service.spreadsheets().values()
+        self.id = sheet_id
+
+
+    def read(self, rows, cols, sheet=1):
+        if type(cols[0]) is int:
+            col1 = ''.join([string.ascii_uppercase[cols[0] % 26]] * (cols[0] / 26))
+            col2 = ''.join([string.ascii_uppercase[cols[1] % 26]] * (cols[1] / 26))
+        else: 
+            col1, col2 = cols
+        row1, row2 = rows
+        ranges = 'Sheet%d!%s%d:%s%d' % (sheet, col1, row1, col2, row2)
+
+        return self.sheet.get(spreadsheetId=self.id, 
+                              range=ranges).execute().get('values', [])
+    
+
+    def write(self, data, sheet=1): 
+        self.sheet.append(spreadsheetId=self.id, 
+                          valueInputOption='RAW',
+                          insertDataOption='INSERT_ROWS',
+                          range='Sheet%s'%sheet, 
+                          body={"values": data}).execute()
+
+
+if __name__ == '__main__':
+    spreadsheetId = '1Ed_tyOHhyc-BAPKkcXJRD_R5xZbbb93aPYxNQl8wYbg'
+
+    data =  [
+        ["test_name", "test_ta", "test_comment", "test_1", "test_2", "test_3", "test_4", "test_5", "test_6", "test_7", "test_8", '', 'test_notes'],
+        ["test2_name", "test2_ta", "test2_comment", "test2_1", "tes2t_2", "test2_3", "test2_4", "test2_5", "test2_6", "test2_7", "test2_8", '', 'test2_notes'],
+    ]
+    sheet = Sheet(spreadsheetId)
+    for row in sheet.read((1,5), ('A','M')):
+        print row
+    print
+
+    sheet.write(data)
+
+    for row in sheet.read((1,5), ('A','M')):
+        print row
---
+++ submit-assignments.py
@@ -0,0 +1,50 @@
+from google_spreadsheet import Sheet
+from glob import glob
+from os.path import exists
+from openpyxl import load_workbook
+
+SHEET_ID   = '1Ed_tyOHhyc-BAPKkcXJRD_R5xZbbb93aPYxNQl8wYbg'
+ASSIGNMENT = 'project 1'
+TA_NAME = ''
+
+def submit():
+    ''' Submit all grades in the grades.xlsx file to 
+        the google spreadsheet '''
+    assignment = None
+    for folder in glob('./assignments/*/'):
+        if ASSIGNMENT.lower() in folder.lower():
+            assignment = folder.split('\\')[1]
+            break
+    if assignment is None:
+        raise Exception('Assignment %s not found in the assignments folder.' % ASSIGNMENT)
+
+    filename = './assignments/%s/grades.xlsx' % assignment
+    if not exists(filename):
+        raise Exception('%s does not exist - have you ran pull-assignments.py?'%filename)
+
+    wb = load_workbook(filename)
+    ws = wb.worksheets[0]
+
+    rows = list(ws.iter_rows())[7:]
+    idxs = [0,2,3,4,5,6,7,8,9,11]
+    data = [[r[i].internal_value if r[i].internal_value is not None else '' 
+            for i in idxs] for r in rows if r[0].internal_value]
+    data = [[d[0]] + [TA_NAME] + [d[-1]] + d[1:-1] + [sum([int(v) if v else 0 for v in d[1:-1]])]
+            for d in data]
+    data = [[str(d) for d in row] for row in data]
+
+    sheet = Sheet(SHEET_ID)
+    sheet.write(data)
+    
+    
+def main():
+    assert(TA_NAME), 'Need to enter your TA Name in this script'
+    resp = raw_input('Submit current grades with:\nGoogle spreadsheet:'+
+                     ' %s\nAssignment: %s\nTA: %s \ny/n?'%(SHEET_ID, ASSIGNMENT, TA_NAME))
+    if resp != 'y': return
+    submit()
+
+
+
+
+if __name__ == '__main__': main()
